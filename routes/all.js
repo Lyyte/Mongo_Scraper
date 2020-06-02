173b867032edc20dfc719b4bb752af70b531985f
Lyyte <lyytefaridi@gmail.com>
2020-06-01 20:24:23 -0500

    added route to show all articles, and to show note per article

--- routes/all.js
+++ routes/all.js
@@ -1,22 +1,21 @@
 // Routes
 const express = require('express'),
       cheerio = require('cheerio'),
-      db = require('../models');
-      axios = require('axios')
-
+      db = require('../models'),
+      axios = require('axios');
 
 module.exports = function(app) {
-app.get("/scrape", function(req, res) {
-    axios.get("https://www.nytimes.com/section/us").then(function(response) {
+app.get('/scrape', function(req, res) {
+    axios.get('https://www.nytimes.com/section/us').then(function(response) {
       var $ = cheerio.load(response.data);
-        $("article h2").each(function(i, element) {
+        $('article h2').each(function(i, element) {
         var result = {};
           result.title = $(this)
-          .children("a")
+          .children('a')
           .text();
         result.link = $(this)
-          .children("a")
-          .attr("href");
+          .children('a')
+          .attr('href');
   
         db.Article.create(result)
           .then(function(dbArticle) {
@@ -26,22 +25,28 @@ app.get("/scrape", function(req, res) {
             console.log(err);
           });
       });
-      res.send("Scraped");
+      res.send('Scraped');
     });
   });
-  // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
+app.get('/articles', function(req, res) {
     db.Article.find({})
       .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
+app.get('/articles/:id', function(req, res) {
+  db.Article.findOne({ _id: req.params.id})
+    .populate('Note')
+    .then(function(dbArticle){
+      res.json (dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
 
 }
